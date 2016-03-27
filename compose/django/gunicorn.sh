605dd46b6457ed13010db9a01ffd6f9a21b5220f
ryan <rsgm123@gmail.com>
2016-03-27 13:06:56 -0500

    Changed gunicorn timeout, 0.0.17

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -2,4 +2,4 @@
 python /app/manage.py compress
 python /app/manage.py collectstatic --noinput
 python /app/manage.py check_permissions
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app --log-level debug
+/usr/local/bin/gunicorn config.wsgi -w 3 -b 0.0.0.0:5000 --chdir=/app --timeout 90 --log-level debug
