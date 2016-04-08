bd864dfa54bb8e942f01559024427996b67909a8
ryan <rsgm123@gmail.com>
2016-04-08 18:44:42 -0500

    Changed gunicorn to an async worker

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
 python /app/manage.py collectstatic_js_reverse
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compress
 python /app/manage.py check_permissions
-/usr/local/bin/gunicorn config.wsgi -w 3 -b 0.0.0.0:5000 --chdir=/app --timeout 90 --log-level info --access-logfile /logs/gunicorn.log
+
+/usr/local/bin/gunicorn config.wsgi \
+    --workers  3 \
+    --worker-class gevent \
+    -b 0.0.0.0:5000 \
+    --chdir=/app \
+    --timeout 90 \
+    --log-level info \
+    --access-logfile /logs/gunicorn.log
