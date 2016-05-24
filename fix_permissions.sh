9b7bcfcc223c830e9c3b8716e892d6f9889a0856
Ryan Mirman <rsgm123@gmail.com>
2016-05-24 11:19:25 -0500

    Fixed permissions again, again

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Set root readonly
-chmod -R 744 ./
+chmod 755 $(find ./ -type d)
+#chmod 644 $(find ./ -type f)
+find ./ -type f -exec chmod 744 {} +
 
-# Set python read and execute
-chmod -R 755 ./speakeazy/
-chmod -R 755 ./config/
-
-# Static files may be readonly
-chmod -R 744 ./speakeazy/templates/
-chmod -R 744 ./speakeazy/static/
 
 # Media files must be writable
-chmod -R 766 ./speakeazy/media/
-chmod -R 766 ./recordings/
+chmod 777 $(find ./speakeazy/media/ -type d)
+chmod 666 $(find ./speakeazy/media/ -type f)
+
+chmod 777 $(find ./speakeazy/media/ -type d)
+chmod 666 $(find ./recordings/ -type f)
+
 
 # One off files that need read write
-chmod -R 766 ./speakeazy.db
-chmod -R 766 ./speakeazy/static/css/
-chmod -R 766 ./recordings
+chmod 766 ./speakeazy.db
+chmod 777 ./speakeazy/static/css/
+chmod 766 ./speakeazy/static/js/reverse.js
+
 
 # Obviously these need execute
-chmod -R 755 ./compose/**/*.sh
+chmod 755 ./compose/**/*.sh
+
