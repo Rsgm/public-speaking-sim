1d126a878b83e76b38f6f2acd90320300de1ed14
Ryan <rsgm123@gmail.com>
2016-06-13 14:13:32 -0500

    Clear comment form on submission

--- speakeazy/static/js/comment_form.js
+++ speakeazy/static/js/comment_form.js
@@ -1,18 +1,20 @@
 (function () {
   $('.comment-form button').click(function () {
-    var text = $('.comment-form textarea').val();
+    var $input = $('.comment-form textarea');
+    var text = $input.val();
     $.post(Urls['recordings:recording:comments:create'](se.authorization.type, se.authorization.key), {
       text: text,
       csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
     }, function () {
+      $input.val('');
       var $list = $('.uk-comment-list');
       var $comment = $('<li class="uk-comment">' +
-          '<header class="uk-comment-header">' +
-          '<h4 class="uk-comment-title">' + se.user + '</h4>' +
-          '<div class="uk-comment-meta">' + moment().format('MMMM D, YYYY, h:mm a') + '</div>' +
-          '</header>' +
-          '<div class="uk-comment-body">' + text + '</div>' +
-          '</li>');
+        '<header class="uk-comment-header">' +
+        '<h4 class="uk-comment-title">' + se.user + '</h4>' +
+        '<div class="uk-comment-meta">' + moment().format('MMMM D, YYYY, h:mm a') + '</div>' +
+        '</header>' +
+        '<div class="uk-comment-body">' + text + '</div>' +
+        '</li>');
       $list.append($comment);
     });
   });
