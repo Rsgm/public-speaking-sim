7c56e25346a01edf167c51e3c6d461774fef74db
Ryan Mirman <rsgm123@gmail.com>
2016-07-07 20:55:14 -0500

    Added certbot public egress to nginx server

--- devops/terraform.tf
+++ devops/terraform.tf
@@ -211,6 +211,15 @@ resource "aws_security_group" "nginx" {
       "${var.django_ip}/32"
     ]
   }
+  // Allow 443 traffic for certbot - PUBLIC
+  egress {
+    from_port = "443"
+    to_port = "443"
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
   tags {
     Name = "nginx"
   }
