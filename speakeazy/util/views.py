b8150dbfcb06abe4122550825aff942af6cdcc93
Ryan Mirman <rsgm123@gmail.com>
2016-07-21 18:27:37 -0500

    Changed PostView base class

--- speakeazy/util/views.py
+++ speakeazy/util/views.py
@@ -1,8 +1,13 @@
 from django.http.response import Http404
-from vanilla.views import GenericView
+from django.views.generic.base import View
 
 
-class PostView(GenericView):
+class PostView(View):
+    """
+    A simple class based view for post resources.
+
+    It may be a good idea to replace this with an actual rest framework. Preferably a lightweight framework.
+    """
     http_method_names = ['post']
 
     def get(self):
